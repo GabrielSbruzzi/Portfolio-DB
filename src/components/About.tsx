6380aa65838daef1afe4049594edb0594237348c
Gabriel Sbruzzi <gabriel.sbruzzi@dbserver.com.br>
2025-01-31 16:50:53 -0300

    Refatoração do Portfolio v2

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,7 +7,7 @@ const About: React.FC = () => {
         <section className="container">
             <div className="about">
                 <h2>Sobre mim</h2>
-                <p>Sou trainee de desenvolvimento de software na DBServer. Amo aprender novas tecnologias e aplicá-las para resolver problemas. Sempre busco me aprimorar e colaborar com o sucesso da equipe.</p>
+                <p>Sou trainee de Desenvolvimento de Software na DBServer, estudo Análise e Desenvolvimento de Sistemas e, ao longo da minha trajetória, busquei aprimorar minhas habilidades por meio de formações técnicas. Em 2022, concluí um curso técnico de Programação Web, onde tive contato com linguagens de back-end, como PHP. Em 2023, aprofundei meus conhecimentos em análise de dados ao realizar um curso técnico de Data Science, adquirindo experiência em automação com Python, Power BI e SQL. Tenho grande interesse na área de QA e pretendo atuar profissionalmente nesse campo, aplicando as melhores práticas para garantir a qualidade do software e contribuir para o desenvolvimento de soluções escaláveis, eficientes e de alto impacto.</p>
             </div>
             <Skills/>
         </section>
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -10,26 +10,26 @@ const Skills: React.FC = () => {
                 <div className="backEnd-container">
                     <div className="ul-skills-backend">
                         <ul>
-                            <li>Python</li>
-                            <li>SPRINGBOOT</li>
+                            <li>SPRING BOOT</li>
                             <li>H2</li>
                             <li>Swagger</li>
                             <li>JUnit</li>
-                            <li>Cypress</li>
-                            <li><span>JAVA</span></li>
+                            <li>RestAssured</li>
+                            <li>JAVA</li>
                             <li>SQL</li>
+                            <li>Python</li>
                         </ul>
                     </div>
                     <div className="imgs-skills">
                         <ul>
+                            <li><img src="/status/Frame%204.png" alt="Imagem status"/></li>
+                            <li><img src="/status/Frame%204.png" alt="Imagem status"/></li>
+                            <li><img src="/status/Frame%204.png" alt="Imagem status"/></li>
+                            <li><img src="/status/Frame%204.png" alt="Imagem status"/></li>
+                            <li><img src="/status/Frame%204.png" alt="Imagem status"/></li>
+                            <li><img src="/status/Frame%204.png" alt="Imagem status"/></li>
+                            <li><img src="/status/Frame%204.png" alt="Imagem status"/></li>
                             <li><img src="/status/Frame%206.png" alt="Imagem status"/></li>
-                            <li><img src="/status/Frame%204.png" alt="Imagem status"/></li>
-                            <li><img src="/status/Frame%204.png" alt="Imagem status"/></li>
-                            <li><img src="/status/Frame%204.png" alt="Imagem status"/></li>
-                            <li><img src="/status/Frame%204.png" alt="Imagem status"/></li>
-                            <li><img src="/status/Frame%204.png" alt="Imagem status"/></li>
-                            <li><img src="/status/Frame%204.png" alt="Imagem status"/></li>
-                            <li><img src="/status/Frame%204.png" alt="Imagem status"/></li>
                         </ul>
                     </div>
                 </div>
@@ -39,18 +39,22 @@ const Skills: React.FC = () => {
                 <div className="frontEnd-container">
                     <div className="ul-skills-frontend">
                         <ul>
+                            <li>HTML</li>
+                            <li>Postman</li>
+                            <li>Cypress</li>
+                            <li>CSS</li>
                             <li>REACT</li>
                             <li>NODE</li>
-                            <li>CSS</li>
-                            <li><span>HTML</span></li>
                         </ul>
                     </div>
                     <div className="imgs-skills">
                         <ul>
-                            <li><img src="/status/Frame%205.png" alt="Imagem status"/></li>
-                            <li><img src="/status/Frame%205.png" alt="Imagem status"/></li>
-                            <li><img src="/status/Frame%204.png" alt="Imagem status"/></li>
                             <li><img src="/status/Frame%203.png" alt="Imagem status"/></li>
+                            <li><img src="/status/Frame%204.png" alt="Imagem status"/></li>
+                            <li><img src="/status/Frame%204.png" alt="Imagem status"/></li>
+                            <li><img src="/status/Frame%204.png" alt="Imagem status"/></li>
+                            <li><img src="/status/Frame%205.png" alt="Imagem status"/></li>
+                            <li><img src="/status/Frame%205.png" alt="Imagem status"/></li>
                         </ul>
                     </div>
                 </div>
